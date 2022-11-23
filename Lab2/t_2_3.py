f8ca276a0d2ddd250f60c6f752557e6a9b0b0b71
Donistr <deniselag@mail.ru>
2022-11-23 22:19:46 +0400

    соединил две функции разбиения данных на года и недели(для этого сделал первый enum); соединил две функции поиска данных в файлах с датами и значениями(для этого сделал второй enum); соединил 3 функции поиска данных в изначальном файле, файле разделённом по годам, файле разделённом по неделям(для этого использую второй enum)

---
+++ Lab2/t_2_3.py
@@ -0,0 +1,64 @@
+from t_1 import write_string_to_csv
+from t_1 import read_file
+import enum
+
+
+class FunctionState(enum.Enum):
+    partition_years = 1
+    partition_weeks = 2
+
+
+def partition_data_n_files_years_or_weeks(type_file: FunctionState, data_file_name: str = 'data_file.csv') -> None:
+    """
+    функция разделяет исходный массив данных на n файлов, где каждый файл содержит данные за конкретный год
+    или неделю, каждый файл будет назван так: годмесяцдень_годмесяцдень, по первой и последней датам в файле
+    type_file - параметр, который контролирует разбивает функция файлы по годам или по неделям
+    data_file_name - название файла с данными
+    """
+    data_list = read_file(data_file_name)
+    last_date = data_list[0]['date']
+    if type_file == FunctionState.partition_years:
+        last_year = last_date.year
+    else:
+        if type_file == FunctionState.partition_weeks:
+            last_week = last_date.isocalendar()[1]
+    array = list()
+    for item in data_list:
+        tmp_date = item['date']
+        if type_file == FunctionState.partition_years:
+            if tmp_date.year == last_year:
+                array.append(item)
+            else:
+                file_name = f'{last_year}{array[-1]["date"].month:02d}{array[-1]["date"].day:02d}_' \
+                            f'{last_year}{array[0]["date"].month:02d}{array[0]["date"].day:02d}.csv'
+                for i in range(len(array)):
+                    write_string_to_csv(f'{array[i]["date"]}, {array[i]["value"]}', file_name)
+                last_year = tmp_date.year
+                array.clear()
+                array.append(item)
+        else:
+            if type_file == FunctionState.partition_weeks:
+                if tmp_date.isocalendar()[1] == last_week:
+                    array.append(item)
+                else:
+                    file_name = f'{array[-1]["date"].year}{array[-1]["date"].month:02d}{array[-1]["date"].day:02d}_' \
+                                f'{array[0]["date"].year}{array[0]["date"].month:02d}{array[0]["date"].day:02d}.csv'
+                    for i in range(len(array)):
+                        write_string_to_csv(f'{array[i]["date"]}, {array[i]["value"]}', file_name)
+                    last_week = tmp_date.isocalendar()[1]
+                    array.clear()
+                    array.append(item)
+    if type_file == FunctionState.partition_years:
+        file_name = f'{last_year}{array[-1]["date"].month:02d}{array[-1]["date"].day:02d}_' \
+                    f'{last_year}{array[0]["date"].month:02d}{array[0]["date"].day:02d}.csv'
+    else:
+        if type_file == FunctionState.partition_weeks:
+            file_name = f'{array[-1]["date"].year}{array[-1]["date"].month:02d}{array[-1]["date"].day:02d}_' \
+                        f'{array[0]["date"].year}{array[0]["date"].month:02d}{array[0]["date"].day:02d}.csv'
+    for i in range(len(array)):
+        write_string_to_csv(f'{array[i]["date"]}, {array[i]["value"]}', file_name)
+
+
+if __name__ == "__main__":
+    partition_data_n_files_years_or_weeks(FunctionState.partition_years)
+    partition_data_n_files_years_or_weeks(FunctionState.partition_weeks)
--- Lab2/t_4.py
+++ Lab2/t_4.py
@@ -4,59 +4,74 @@ from os import listdir
 import csv
 import logging
 import re
+import enum
 
 
-def get_value_from_0_type_file(required_date: datetime, data_file_name: str = 'data_file.csv') -> [float, None]:
+class FunctionState(enum.Enum):
+    date_file = 1
+    value_file = 2
+    type_file_0 = 3
+    type_file_2 = 4
+    type_file_3 = 5
+
+
+def get_value_from_0_2_3_type_file(required_date: datetime, type_file: FunctionState,
+                                   data_file_0_type_name: str = 'data_file.csv') -> [float, None]:
     """
-    функция ищет в исходном файле данные за конкретный день, если они есть - возвращает данные за этот день, если нет -
-    возвращает None
+    функция ищет конкретную дату в файле, в котором хранятся данные, если она есть - возвращает данные за этот день из
+    файла с данными, если нет - возвращает None
     required_date - дата, для которой ищем данные
-    data_file_name - название файла с данными
+    type_file - параметр, который контролирует из файла какого типа будут считываться данные
+    data_file_0_type_name - название файла 0 типа
     """
-    data_list = read_file(data_file_name)
-    array_date = list()
-    array_val = list()
-    for item in data_list:
-        array_date.append(item['date'])
-        array_val.append(item['value'])
-    if required_date in array_date:
-        required_index = array_date.index(required_date)
-        return array_val[required_index]
+    file_name = ''
+    if type_file == FunctionState.type_file_0:
+        file_name = data_file_0_type_name
     else:
+        if type_file == FunctionState.type_file_2 or type_file == FunctionState.type_file_3:
+            file_names = listdir()
+            for item in file_names:
+                if '.csv' in item:
+                    if f'{required_date.year}' in item:
+                        date_1_str = item.split('_')[0]
+                        date_2_str = item.split('_')[1].split('.')[0]
+                        date_1 = datetime.date(int(date_1_str[:4]), int(date_1_str[4:6]), int(date_1_str[6:8]))
+                        date_2 = datetime.date(int(date_2_str[:4]), int(date_2_str[4:6]), int(date_2_str[6:8]))
+                        if type_file == FunctionState.type_file_2 and date_2 - date_1 > datetime.timedelta(7):
+                            file_name = item
+                        else:
+                            if type_file == FunctionState.type_file_3 and date_2 - date_1 < datetime.timedelta(7) and \
+                                    date_1 <= required_date <= date_2:
+                                file_name = item
+    if file_name == '':
         return None
+    data_list = read_file(file_name)
+    for obj in data_list:
+        if required_date == obj['date']:
+            return obj['value']
+    return None
 
 
-def read_date_file(file_name: str = 'data_file.csv') -> list:
+def read_date_or_value_file(file_name: str, type_file: FunctionState) -> list:
     """
-    функция считывает данные из csv файла
+    функция считывает дыти или значения из файла
     file_name - название файла, из которого считываем
+    type_file - параметр, который контролирует считывает функция данный из файла с датами или из файла со значениями
     """
     try:
         with open(file_name, newline='') as file:
             reader = csv.reader(file)
             data = list()
             for row in reader:
-                str_date = re.match('\d{4}-\d\d-\d\d', row[0]).group(0)
-                tmp_date = datetime.datetime.strptime(str_date, '%Y-%m-%d').date()
-                data.append(tmp_date)
-            return data
-    except OSError as error:
-        logging.error(f'Ошибка, не удалось открыть файл: {error}')
-
-
-def read_value_file(file_name: str = 'data_file.csv') -> list:
-    """
-    функция считывает данные из csv файла
-    file_name - название файла, из которого считываем
-    """
-    try:
-        with open(file_name, newline='') as file:
-            reader = csv.reader(file)
-            data = list()
-            for row in reader:
-                str_value = re.match('(?:\d*\.\d+|\d+)', row[0]).group(0)
-                tmp_value = float(str_value)
-                data.append(tmp_value)
+                if type_file == FunctionState.date_file:
+                    str_date = re.match('\d{4}-\d\d-\d\d', row[0]).group(0)
+                    tmp_date = datetime.datetime.strptime(str_date, '%Y-%m-%d').date()
+                    data.append(tmp_date)
+                else:
+                    if type_file == FunctionState.value_file:
+                        str_value = re.match('(?:\d*\.\d+|\d+)', row[0]).group(0)
+                        tmp_value = float(str_value)
+                        data.append(tmp_value)
             return data
     except OSError as error:
         logging.error(f'Ошибка, не удалось открыть файл: {error}')
@@ -65,65 +80,19 @@ def read_value_file(file_name: str = 'data_file.csv') -> list:
 def get_value_from_1_type_file(required_date: datetime, data_file_name_1: str = 'X.csv',
                                data_file_name_2: str = 'Y.csv') -> [float, None]:
     """
-    функция ищет конкретную дату в файле, котором хранятся данные, если она есть - возвращает данные за этот день из
+    функция ищет конкретную дату в файле, в котором хранятся данные, если она есть - возвращает данные за этот день из
     файла с данными, если нет - возвращает None
     required_date - дата, для которой ищем данные
     data_file_name_1 - имя файла с датами
     data_file_name_2 - имя файла с данными
     """
-    data_list_x = read_date_file(data_file_name_1)
-    data_list_y = read_value_file(data_file_name_2)
+    data_list_x = read_date_or_value_file(data_file_name_1, FunctionState.date_file)
+    data_list_y = read_date_or_value_file(data_file_name_2, FunctionState.value_file)
     if required_date in data_list_x:
         required_index = data_list_x.index(required_date)
         return data_list_y[required_index]
     else:
         return None
-
-
-def get_value_from_2_type_file(required_date: datetime) -> [float, None]:
-    """
-    функция ищет файл, содержащий данные за год, указанный в искомой дате, считывает его и ищет в нём данные для искомой
-    даты, если они есть - возвращает их, если нет - возвращает None
-    required_date - дата, для которой ищем данные
-    """
-    file_names = listdir()
-    for item in file_names:
-        if '.csv' in item:
-            if f'{required_date.year}' in item:
-                date_1_str = item.split('_')[0]
-                date_2_str = item.split('_')[1].split('.')[0]
-                date_1 = datetime.date(int(date_1_str[:4]), int(date_1_str[4:6]), int(date_1_str[6:8]))
-                date_2 = datetime.date(int(date_2_str[:4]), int(date_2_str[4:6]), int(date_2_str[6:8]))
-                if date_2 - date_1 > datetime.timedelta(7):
-                    file_name = item
-                    data_list = read_file(file_name)
-                    for obj in data_list:
-                        if required_date == obj['date']:
-                            return obj['value']
-    return None
-
-
-def get_value_from_3_type_file(required_date: datetime) -> [float, None]:
-    """
-    функция ищет файл, который содержит данные за тот год и за ту неделю, которые содержат искомую дату, считывает его
-    и ищет в нём данные для искомой даты, если они есть - возвращает их, если нет - возвращает None
-    required_date - дата, для которой ищем данные
-    """
-    file_names = listdir()
-    for item in file_names:
-        if '.csv' in item:
-            if f'{required_date.year}' in item:
-                date_1_str = item.split('_')[0]
-                date_2_str = item.split('_')[1].split('.')[0]
-                date_1 = datetime.date(int(date_1_str[:4]), int(date_1_str[4:6]), int(date_1_str[6:8]))
-                date_2 = datetime.date(int(date_2_str[:4]), int(date_2_str[4:6]), int(date_2_str[6:8]))
-                if date_2 - date_1 < datetime.timedelta(7) and date_1 <= required_date <= date_2:
-                    file_name = item
-                    data_list = read_file(file_name)
-                    for obj in data_list:
-                        if required_date == obj['date']:
-                            return obj['value']
-    return None
 
 
 def next(counter: int, data_file_name: str = 'data_file.csv') -> (datetime, float):
@@ -138,7 +107,7 @@ def next(counter: int, data_file_name: str = 'data_file.csv') -> (datetime, floa
 
 if __name__ == "__main__":
     date = datetime.date(2022, 10, 15)
-    print(get_value_from_0_type_file(date))
+    print(get_value_from_0_2_3_type_file(date, FunctionState.type_file_0))
     print(get_value_from_1_type_file(date))
-    print(get_value_from_2_type_file(date))
-    print(get_value_from_3_type_file(date))
+    print(get_value_from_0_2_3_type_file(date, FunctionState.type_file_2))
+    print(get_value_from_0_2_3_type_file(date, FunctionState.type_file_3))
